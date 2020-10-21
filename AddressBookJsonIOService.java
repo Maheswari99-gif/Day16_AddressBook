f88bc2190dab0cf220167a6396f17ac243e27bdd
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-21 19:05:29 +0530

    MaheswariKoyyani : Create and Read files using Json

---
+++ AddressBookJsonIOService.java
@@ -0,0 +1,47 @@
+package com.capgemini.bookaddress;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+
+public class AddressBookJsonIOService {
+	public String addressBookJsonFileName;
+
+	public AddressBookJsonIOService(String addressBookJsonFileName) {
+		super();
+		this.addressBookJsonFileName = addressBookJsonFileName;
+	}
+
+	public void writeContactDetails(List<ContactDetails> contacts) {
+		try {
+			Gson gson = new Gson();
+			String json = gson.toJson(contacts);
+			FileWriter writer = new FileWriter(this.addressBookJsonFileName);
+			writer.write(json);
+			writer.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public List<ContactDetails> readContactDetails() {
+		List<ContactDetails> contacts = null;
+		try {
+			Gson gson = new Gson();
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(this.addressBookJsonFileName));
+			ContactDetails[] contactsArray = gson.fromJson(bufferedReader, ContactDetails[].class);
+			contacts = new LinkedList<ContactDetails>(Arrays.asList(contactsArray));
+			bufferedReader.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return contacts;
+	}
+
+}
